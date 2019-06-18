be79c7302c0dedc31192041304f5090ce00cd717
Jérémy Thoveron <jeremy.thoveron-@epitech.eu>
2019-06-18 17:09:15 +0200

    CHange to have personalized table

--- database/db.py
+++ database/db.py
@@ -11,35 +11,61 @@ def init_db():
 
         return db
     except pymysql.Error as e:
-        print("DB connection failed: "+str(e))
+        print("DB connection failed: " + str(e))
 
 
 def close_db(db):
     db.close()
 
 
-def create_table(db, table_name):
+def create_table(db, table_name, vars):
     cur = db.cursor()
-    part1 = "CREATE TABLE IF NOT EXISTS "
-    part2 =" (id INT(10) PRIMARY KEY AUTO_INCREMENT, datas  VARCHAR(255) NOT NULL, tag TINYINT NOT NULL);"
-    sql = part1 + table_name + part2
+    sql = "CREATE TABLE IF NOT EXISTS " + table_name + " (id INT(10) PRIMARY KEY AUTO_INCREMENT"
+    for a, b in vars:
+        sql += ", " + a + " " + b + " NOT NULL"
+    sql += ')'
     try:
         cur.execute(sql)
     except pymysql.Error as e:
         print("Create table failed" + str(e))
 
 
-def drop_table(db):
+def drop_table(db, table):
+    sql = "DROP TABLE " + table
     try:
         cur = db.cursor()
-        cur.execute("DROP TABLE imgs")
+        cur.execute(sql)
         db.commit()
     except pymysql.Error as e:
         print("Drop table  failed: " + str(e))
 
 
-def put_path(db, table, path):
-    sql = "INSERT INTO " + table + " (datas, tag) VALUES (\'" + path + "\', 0)"
+def put_path(db, table, path, vars):
+    var_name = []
+    var_value = []
+    for a, b in vars:
+        var_name.append(a)
+        var_value.append(b)
+
+    sql = "INSERT INTO " + table + " ("
+    for i in var_name:
+        if i == var_name[-1]:
+            sql += i
+        else:
+            sql += i + ", "
+    sql += ") VALUES ("
+    for i in var_value:
+        if i == var_value[-1]:
+            if type(i) is int:
+                sql += str(i)
+            elif type(i) is str:
+                sql += "\'" + i + "\'"
+        else:
+            if type(i) is int:
+                sql += str(i) + ", "
+            elif type(i) is str:
+                sql += "\'" + i + "\', "
+    sql += ')'
     try:
         cur = db.cursor()
         cur.execute(sql)
@@ -59,9 +85,9 @@ def get_path(db, table):
         print("Fetch data failed: " + str(e))
 
 
-def delete_img(db, img_id):
+def delete_img(db, img_id, table):
     cur = db.cursor()
-    sql = "DELETE FROM imgs WHERE id = '%d'" % img_id
+    sql = "DELETE FROM " + table + "WHERE id = '%d'" % img_id
     try:
         cur.execute(sql)
         db.commit()
@@ -71,7 +97,7 @@ def delete_img(db, img_id):
 
 def is_training(db, path, table_name):
     cur = db.cursor()
-    sql = "UPDATE " + table_name + " SET tag = 1 WHERE datas = \'" + path + "\';"
+    sql = "UPDATE " + table_name + " SET tag = 1 WHERE datas = \'" + path + "\'"
     try:
         cur.execute(sql)
         db.commit()
